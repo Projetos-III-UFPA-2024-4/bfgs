14cfd096bd98933c1e0d2e81e0e3d44d08de0329
Wellerson Nascimento <wellerson.veloso10@gmail.com>
2025-01-19 04:00:45 -0300

    Mudança na vizualização do diagrama

--- Diagrams/Diagrams.py
+++ Diagrams/Diagrams.py
@@ -2,25 +2,28 @@ from diagrams import Cluster, Diagram
 from diagrams.custom import Custom
 from diagrams.aws.database import DatabaseMigrationService, DMS
 from diagrams.aws.general import Client, User
+from diagrams.aws.compute import Lambda
 
-with Diagram("Sistema dinamico de Semaforos", show=False):
-    api = Custom("TomTom or ...", r".\resources\API_2.png")
-    db = DMS("Database")
+with Diagram("Sistema dinamico de Semaforos", show=False, node_attr={"fontsize": "16", "fontname": "Arial"}):
+    with Cluster("API", graph_attr={"bgcolor": "lightblue"}):
+        tomtom = Custom("", r".\resources\TomTomSymbol.png")
+        another_api = Custom("Another API", r".\resources\API.png")
+        api = [tomtom, another_api]
 
-    with Cluster("Cloud"):
-        aws = Custom("AWS services", r".\resources\AWS.png")
+    with Cluster("Cloud", graph_attr={"bgcolor": "#00CC99"}):
+        lambda_function = Lambda("Algoritm")
+        db = DMS("Database")
 
-    with Cluster("Simulation"):
-       sumo = Custom("Sumo", r".\resources\SUMO.png")
-       traffic = Custom("Traffic Light", r".\resources\traffic-light.png")
-       simul = [sumo, traffic]
+    with Cluster("Simulation", graph_attr={"bgcolor": "#FFCC66"}):
+        sumo = Custom("", r".\resources\SUMO.png")
+        traffic = Custom("Traffic Light", r".\resources\traffic-light.png")
+        simul = [sumo, traffic]
 
-    with Cluster("Interface"):
+    with Cluster("Interface", graph_attr={"bgcolor": "#FFFF99"}):
         monitor = Client("Monitor")
         user = User("User")
-        monitor >> user
 
-    api >> db
-    db >> aws
-    aws >> simul
-    aws >> monitor
+    api >> db >> lambda_function
+    lambda_function >> simul
+    lambda_function >> monitor >> user
+    user >> monitor >> lambda_function
