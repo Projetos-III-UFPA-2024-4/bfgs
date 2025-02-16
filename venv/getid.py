9a6e12d3690fa80aab3c9fe6d24e347fdb73a658
Wuell <wellerson.veloso10@gmail.com>
2025-02-15 23:41:20 -0300

    Adicionando comentarios ao arquivo getid.py

--- venv/getid.py
+++ venv/getid.py
@@ -1,16 +1,18 @@
+#Criei esse code para podemos pegar os ids de algo que a gente precise
 import traci
 
 # Inicie o SUMO com TraCI
 traci.start(["sumo", "-c", r'data\myintersection.sumocfg'])
 
-# Pegando todos os IDs dos semáforos
 
+# Pegando todos os IDs dos semáforos
 def getIDtls():
     return print("IDs dos semáforos:", traci.trafficlight.getIDList())
 
+# Pegando todos os IDs das vias
 def getIdedges():
     return print("IDs das Vias:", traci.edge.getIDList())
 
-getIdedges()
+
 # Finaliza a simulação
 traci.close()
