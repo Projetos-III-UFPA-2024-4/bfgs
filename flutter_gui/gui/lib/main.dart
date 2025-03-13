93e06c3d5f219329c00bda0984c9c34b4e51631a
Heitor Pereira <heytorpg7@gmail.com>
2025-03-12 21:38:48 -0300

    added a ton of sht

--- flutter_gui/gui/lib/main.dart
+++ flutter_gui/gui/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
+import 'package:gui/wigets/flutter_map_custom.dart';
 
 void main() {
   runApp(MyApp());
@@ -27,61 +26,26 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class MyAppState extends ChangeNotifier {
+  // todo: estados arquivados no db
+}
+
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
 
   @override
-  _MapScreenState createState() => _MapScreenState();
+  State<MapScreen> createState() => _MapScreenState();
 }
 
 class _MapScreenState extends State<MapScreen> {
-  final LatLng _initialPosition = LatLng(-1.45502, -48.5024);
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Row(
         children: [
-          Expanded(
-            child: FlutterMap(
-              options: MapOptions(initialCenter: _initialPosition),
-              children: [
-                TileLayer(
-                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                  userAgentPackageName: 'com.example.app',
-                ),
-               MarkerLayer(
-                markers: [
-                  Marker(
-                  child: ElevatedButton(onPressed: () { print('l1'); }, child: null),
-                  point: LatLng(-1.412062, -48.442478), //-1.412062/-48.442478
-                  width: 80,
-                  height: 80
-                  ),
-                  Marker(
-                  child: ElevatedButton(onPressed: () { print('l2'); }, child: null),
-                  point: LatLng(-1.412373, -48.442162), // -1.412373/-48.442162
-                  width: 80,
-                  height: 80
-                  ),
-                  Marker(
-                  child: ElevatedButton(onPressed: () { print('l3'); }, child: null),
-                  point: LatLng(-1.412029, -48.442248), // -1.412029/-48.442248
-                  width: 80,
-                  height: 80
-                  ),
-                  Marker(
-                  child: ElevatedButton(onPressed: () { print('l4'); }, child: null),
-                  point: LatLng(-1.412359, -48.442626), // -1.412359/-48.442626
-                  width: 80,
-                  height: 80
-                  ),
-    ],
-)
-              ],
-            ),
-          ),
-
+          Expanded(child: FlutterMapCustom()),
           Container(
             width: 250,
             color: Colors.green[100],
@@ -100,10 +64,6 @@ class _MapScreenState extends State<MapScreen> {
                 ),
               ),
             ),
-          ),
-
-          BottomAppBar(
-            // child: TerminalView()
           ),
         ],
       ),
