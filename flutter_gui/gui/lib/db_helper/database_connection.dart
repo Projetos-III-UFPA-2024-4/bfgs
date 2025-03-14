6e02bbdfd068cc33e0860039dc1cf37e9df0a730
Heitor Pereira Soares <heytorpg7@gmail.com>
2025-03-14 16:39:13 -0300

    added code for database auth

--- flutter_gui/gui/lib/db_helper/database_connection.dart
+++ flutter_gui/gui/lib/db_helper/database_connection.dart
@@ -4,13 +4,46 @@ import "package:mysql_client/mysql_client.dart";
 import "package:http/http.dart" as http;
 
 
-Future connect(params) async {
-    final conn  = await MySQLConnection.createConnection(host: dotenv.env['DBHOST'], port: dotenv.env['DBPORT'] as int, userName:  dotenv.env['DBUSER']!, password: dotenv.env['DBPASSWORD']!, databaseName: dotenv.env['DBNAME'] );
 
-    return conn;
-}
+class DatabaseConnection {
 
-Future read(params) async {
+
     
     
+
+
+
+
+
+
+
+    static Future createConnection() async {
+
+        final conn  = await MySQLConnection.createConnection(host: dotenv.env['DBHOST'], port: dotenv.env['DBPORT'] as int, userName:  dotenv.env['DBUSER']!, password: dotenv.env['DBPASSWORD']!, databaseName: dotenv.env['DBNAME'] );
+        
+        conn.connect();
+
+        return conn;
+    }
+
+    static Future read(MySQLConnection connection) async {
+        var result = await connection.execute('SELECT * FROM ${dotenv.env['DBNAME']}');
+
+        for (var value in result.rows) {
+            Map data = value.assoc();
+
+            print(
+                'Id: ${data['id']}'
+            );
+        }
+    }
+
+
+
+
 }
+
+
+
+
+
