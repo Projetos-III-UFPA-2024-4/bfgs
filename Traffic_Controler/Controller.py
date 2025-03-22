e4c7422ece26279946a7230eedee4aaeca73d247
SNesht <101153084+SNesht@users.noreply.github.com>
2025-03-22 03:26:33 -0300

    Ajustes interação db

--- Traffic_Controler/Controller.py
+++ Traffic_Controler/Controller.py
@@ -1,23 +1,41 @@
 import mysql.connector as mc
 
 #initial setup
-def connect(connection_data: tuple):
-    host, user, password, database = connection_data
+def connect(host, user, password, database):
+
     db = mc.connect(
         host=host,
         user=user,
         password=password,
-        database=database,
-        auth_plugin='mysql_native_password'
+        database=database
         )
 
     return db
 
 
 def main():
-    connection_data = ("localhost", "root", "sapos123", "traffic_results")
-    db_gt = connect(connection_data)
+    db_gt = connect(
+        "congestion-state.c02vqeowqrft.us-east-1.rds.amazonaws.com",
+        "root",
+        "bfgs2024",
+        "congestion_state"
+    )
     cursor_selector = db_gt.cursor(buffered=True)
+
+    database = "traffic_updates"
+    sql = f"SELECT * FROM {database}"
+    cursor_selector.execute(sql)
+    print(cursor_selector.rowcount, "record selected.")
+    row_data_raw = cursor_selector.fetchall()
+    print(row_data_raw)
+    n_phases = int(row_data_raw[0][6])
+    print(f'numero de fases {n_phases}')
+    row_data = []
+    for i in range(n_phases):
+        row_data.append({ "phase_number": row_data_raw[i][2], "green_time": row_data_raw[i][4], "cycle": row_data_raw[i][3]})
+
+    for i in range(len(row_data)):
+        print(row_data[i])
 
     cursor_selector.close()
     db_gt.close()
--- Traffic_Optimization/algorithm.py
+++ Traffic_Optimization/algorithm.py
@@ -1,28 +1,28 @@
 import mysql.connector as mc
 
-
 min_green = 15
 sf_default = 1800
+
 
 class Trffclght:
     units = []
 
-    def __init__(self,  n_phases : int, all_critcal_flow : float, of : list, sf : list, edges : list, all_red = 0, lst_phs_time = 2):
+    def __init__(self, n_phases : int, all_critcal_flow : float, of : list, sf : list, phases : list, all_red = 0, lst_phs_time = 2):
         if len(of) != n_phases:
             print("observed flows and number of phases do not match")
             exit(-1)
 
-        self.units.append(self)                             #register the instance of trafficlight
-        self.n_phases = n_phases                            #number of phases
-        self.lost_time = (n_phases * lst_phs_time) + all_red#lost time
-        self.observed_flows = of                                   #latest observed flows
+        self.units.append(self)                                     #register the instance of trafficlight
+        self.n_phases = n_phases                                    #number of phases
+        self.lost_time = (n_phases * lst_phs_time) + all_red        #lost time
+        self.observed_flows = of                                    #latest observed flows
         self.saturation_flows = sf                                  #total saturation flow on the routes
         self.critical_flows_rt = []
         for i in range(len(of)): self.critical_flows_rt.append(of[i] / sf[i])
-        self.all_critical_flows_rt = all_critcal_flow          #summation of all critical flow
-        self.optm_cycle = 0                             #latest cycle caulculated
-        self.green_times = []                               #latest greens calculated
-        self.edges = edges
+        self.all_critical_flows_rt = all_critcal_flow               #summation of all critical flow
+        self.optm_cycle = 0                                         #latest cycle caulculated
+        self.green_times = []                                       #latest greens calculated
+        self.phases = phases
 
     def update(self, all_crtclflw, of, sf):
         self.observed_flows = of #latest observed flows
@@ -53,14 +53,13 @@ def get_greens(crtflws_rt, allcrtflws_rt, ltst_optmcycle, lost_time):
     return greens
 
 
-def connect(connection_data: tuple):
-    host, user, password, database = connection_data
+def connect(host, user, password, database):
+
     db = mc.connect(
         host=host,
         user=user,
         password=password,
-        database=database,
-        auth_plugin='mysql_native_password'
+        database=database
         )
 
     return db
@@ -74,61 +73,77 @@ def fix_fetch(torto: list) -> list:
     return fixed_list
 
 
-def db_update(sinal : Trffclght, cursor, db):
-    sql = "REPLACE INTO results (id, n_phases, optm_cycle, green_times, edges) VALUES (%s, %s, %s, %s, %s)"
+def db_clean(cursor, db, table : str):
+    sql = f"DELETE FROM {table}"
+    cursor.execute(sql)
+    db.commit()
+    print(cursor.rowcount, "record inserted'nt.")
+
+
+def db_update(sinal: Trffclght, cursor, db, table: str):
+    sql = f"INSERT INTO {table} (id, phase_id, cycle_time, green_time, light_state) VALUES (%s, %s, %s, %s, %s)"
     val = []
-    for i in range(len(sinal.edges)):
+    for i in range(len(sinal.phases)):
         val.append(tuple((
             str(i + 1),
-            str(sinal.n_phases),
+            str(i + 1),
             str(sinal.optm_cycle),
             str(sinal.green_times[i]),
-            str(sinal.edges[i]))
-        ))
+            str(sinal.n_phases)
+        )))
     cursor.executemany(sql, val)
     db.commit()
     print(cursor.rowcount, "record inserted.")
+
 def main():
     #initial setup
-    connection_data = ("localhost", "root", "sapos123", "teste")
-    db_gt = connect(connection_data)
+    db_gt = connect(
+        "congestion-state.c02vqeowqrft.us-east-1.rds.amazonaws.com",
+        "root",
+        "bfgs2024",
+        "congestion_state"
+        )
     cursor_selector = db_gt.cursor(buffered=True)
 
-    cursor_selector.execute("SELECT observed_flow FROM traffic_light")
-    of = fix_fetch(cursor_selector.fetchall())
-    #print(of)
+    cursor_selector.execute("SELECT Num_phases FROM congestion_data WHERE cycle_number = 7")
+    num_phases = cursor_selector.fetchone()[0]
 
-    cursor_selector.execute("SELECT num_phases FROM traffic_light")
-    n_phases = cursor_selector.fetchone()[0]
-    #print(n_phases)
+    all_critical_flow = 0
+    observed_flow = []
+    phases = []
 
-    cursor_selector.execute("SELECT critical_flow_total FROM traffic_light")
-    all_crtclflw = cursor_selector.fetchone()[0]
+    cursor_selector.execute("SELECT Phase_Index, Observed_Flow, Critical_Flow, Critical_Flow_TOTAL FROM congestion_data WHERE cycle_number = 7")
+    data = cursor_selector.fetchall()
+    for i in range(num_phases):
+        phases.append(int(data[i][0]/2)+1)
+        observed_flow.append(int(data[i][1]))
+        all_critical_flow = data[0][3]
+
 
     #defines the saturation flow list
     sf = [] #1800 default
-    for i in range(len(of)):
+    for i in range(num_phases):
         sf.append(sf_default)
 
-    cursor_selector.execute("SELECT edge_id FROM traffic_light")
-    edges = fix_fetch(cursor_selector.fetchall())
-    #print(edges)
-
     #sets the trafficlight object
-    sinal = Trffclght(n_phases, all_crtclflw, of, sf, edges, all_red = 12)
-    sinal.update(all_crtclflw, of, sf)
-    #print(sinal.optm_cycle)
-    #print(sinal.green_times)
+    sinal = Trffclght(num_phases, all_critical_flow, observed_flow, sf, phases)
+    sinal.update(all_critical_flow, observed_flow, sf)
 
     cursor_selector.close()
     db_gt.close()
 
     # saving the data
-    connection_data = ("localhost", "sapo", "1234", "traffic_results")
-    db_in = connect(connection_data)
+    db_in = connect(
+        "congestion-state.c02vqeowqrft.us-east-1.rds.amazonaws.com",
+        "root",
+        "bfgs2024",
+        "congestion_state"
+        )
     cursor_in = db_in.cursor(buffered=True)
 
-    db_update(sinal, cursor_in, db_in)
+    db_clean(cursor_in, db_in, "traffic_updates")
+
+    db_update(sinal, cursor_in, db_in, "traffic_updates")
 
     cursor_in.close()
     db_in.close()
