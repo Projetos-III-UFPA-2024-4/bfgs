1c9f04b19af73477cfa5eaf197873e1e5c7f8162
SNesht <101153084+SNesht@users.noreply.github.com>
2025-03-21 21:54:15 -0300

    concluida interação com os banco.

--- Traffic_Optimization/Setup.py
+++ Traffic_Optimization/Setup.py
@@ -1,6 +1,6 @@
 import os # Module provides functions to handle file paths, directories, environment variables
 import sys # Module provides access to Python-specific system parameters and functions
-from Semaforo import Semaforo
+import algorithm as al
 
 # Step 2: Establish path to SUMO (SUMO_HOME)
 if 'SUMO_HOME' in os.environ:
@@ -21,33 +21,53 @@ Sumo_config = [
 ]'''
 
 # Step 5: Open connection between SUMO and Traci
-traci.start(["sumo-gui", "-c", "simulation_config/2025-02-18-01-43-58/osm.sumocfg"])
+traci.start(["sumo-gui", "-c", "teste\dteste.sumocfg"])
 
 # Step 6: Define Variables
 
 #my_edge = 'Via3ToInter' #Aqui é onde mudados para qual via queremos analisar
 
 # Step 7: Define Functions
-
+'''
 def getNumberOfCarsInEdges(edge): #Pega o número de carros em uma via
     num_cars = 0
     if edge in traci.edge.getIDList():
         num_cars = traci.edge.getLastStepVehicleNumber(edge)
         print(f"Número de carros na via {edge}: {num_cars}")
-
+'''
+id = traci.trafficlight.getIDList()
+controlled_lanes = traci.trafficlight.getControlledLanes(id[0])
+phs = 0
+steps = 0
+of_ac = [0,0]
+of = [0,0]
+n_phases = 2
 # Step 8: Take simulation steps until there are no more vehicles in the network traci.simulation.getMinExpectedNumber()
-for i in range(50):
-    traci.simulationStep() # Move simulation forward 1 step
-    id = traci.trafficlight.getIDList()
+for i in range(100):
+    
+    of_ac[0] += traci.edge.getLastStepVehicleNumber(traci.lane.getEdgeID(controlled_lanes[0]))
+    of_ac[1] += traci.edge.getLastStepVehicleNumber(traci.lane.getEdgeID(controlled_lanes[2]))
 
-    print(traci.trafficlight.getPhaseDuration(id[0]))
-    ''' + traci.trafficlight.getNextSwitch(id[0]) - traci.simulation.getTime()'''
-    print(traci.trafficlight.getAllProgramLogics(id[0]))
+    
+    if phs != traci.trafficlight.getPhase(id[0]):
+        of[0] = (of_ac[0]/steps)
+        of[1] = (of_ac[1]/steps)
+        print(of)
+        of_ac = [0,0]
+        steps = 0
+    
+    '''all_crtclflw = 0
+    sf = [1800, 1800] #1800 default
+    for i in range(len(of)):
+        all_crtclflw += of[i]/sf[i]
+    edges = ['a', 'b']
+    lost_time = (2 * 2) + 13
+    sinal = al.Trffclght(n_phases, all_crtclflw, of, sf, edges)'''
 
+    phs = traci.trafficlight.getPhase(id[0])
+    traci.simulationStep()
+    steps += 1
 
-    # Here you can decide what to do with simulation data at each step
-    #getNumberOfCarsInEdges(my_edge)
-    # step_count = step_count + 1
 
 # Step 9: Close connection between SUMO and Traci
 traci.close()
--- Traffic_Optimization/algorithm.py
+++ Traffic_Optimization/algorithm.py
@@ -1,47 +1,138 @@
+import mysql.connector as mc
+
+
+min_green = 15
+sf_default = 1800
+
 class Trffclght:
     units = []
-    def __init__(self,  n_phases : int, all_crtclflw : float, of : list, sf : list, edges : list, all_red = 0, lst_phs_time = 2):
+
+    def __init__(self,  n_phases : int, all_critcal_flow : float, of : list, sf : list, edges : list, all_red = 0, lst_phs_time = 2):
+        if len(of) != n_phases:
+            print("observed flows and number of phases do not match")
+            exit(-1)
+
         self.units.append(self)                             #register the instance of trafficlight
         self.n_phases = n_phases                            #number of phases
         self.lost_time = (n_phases * lst_phs_time) + all_red#lost time
-        #self.ltst_allcrtflw = all_crtclflw                  #summation of all critical flow
-        self.ltst_of = of                                   #latest observed flows
-        self.ltst_sas = sf                                  #total saturation flow on the routes
-        self.ltst_crtflws_rt = []
-        for i in range(len(of)): self.ltst_crtflws_rt.append(of[i]/sf[i])
-        self.ltst_allcrtflws_rt = all_crtclflw
-        self.ltst_optmcycle = 0                             #latest cycle caulculated
-        self.ltst_greens = []                               #latest greens calculated
+        self.observed_flows = of                                   #latest observed flows
+        self.saturation_flows = sf                                  #total saturation flow on the routes
+        self.critical_flows_rt = []
+        for i in range(len(of)): self.critical_flows_rt.append(of[i] / sf[i])
+        self.all_critical_flows_rt = all_critcal_flow          #summation of all critical flow
+        self.optm_cycle = 0                             #latest cycle caulculated
+        self.green_times = []                               #latest greens calculated
         self.edges = edges
 
     def update(self, all_crtclflw, of, sf):
-        #self.latst_crtflw = all_crtclflw #summation of all critical flow
-        self.ltst_ov = of #latest observed flows
-        self.ltst_sas = sf    #total saturation flow on the routes
-        for i in range(len(of)): self.ltst_crtflws_rt.append(of[i]/sf[i])
-        self.ltst_allcrtflws_rt = all_crtclflw
-        self.ltst_optmcycle = get_optmcycle(self.ltst_allcrtflws_rt, self.lost_time)
-        #self.ltst_greens = get_greens(self.ltst_crtflws_rt, self.ltst_allcrtflws_rt, self.ltst_optmcycle, self.lost_time)
+        self.observed_flows = of #latest observed flows
+        self.saturation_flows = sf    #total saturation flow on the routes
+        for i in range(len(of)) : self.critical_flows_rt[i]= of[i] / sf[i] #
+        self.all_critical_flows_rt = all_crtclflw  #summation of all critical flow
+        self.optm_cycle = get_optmcycle(self.all_critical_flows_rt, self.lost_time)
+        self.green_times = get_greens(self.critical_flows_rt, self.all_critical_flows_rt, self.optm_cycle, self.lost_time)
+
 
 def get_optmcycle(allcrtflws_rt, lost_time : int):
-    optmcycle = (1.5 * float(lost_time) + 5) / (1 - allcrtflws_rt)
-    return optmcycle
+    optm_cycle = (1.5 * float(lost_time) + 5) / (1 - allcrtflws_rt)
+
+    return optm_cycle
+
 
 def get_greens(crtflws_rt, allcrtflws_rt, ltst_optmcycle, lost_time):
-
     greens = []
-    for i in len(crtflws_rt):
-        greens.append((crtflws_rt[i]/allcrtflws_rt)*(ltst_optmcycle - lost_time))
-    
+    green = 0
+    for i in range(len(crtflws_rt)):
+        green = (crtflws_rt[i] / allcrtflws_rt) * (ltst_optmcycle - lost_time)
+        if green < min_green:
+            greens.append(min_green)
+            continue
+
+        greens.append(green)
+
     return greens
 
-n_phases = 2
-all_crtclflw = 0.57
-of = [400, 250]
-sf = [1250, 1000]
-edges = ['a', 'b']
 
-sinal = Trffclght(n_phases, all_crtclflw, of, sf, edges)
-#sinal2 = Trffclght(2,2,2,2)
-sinal.update(all_crtclflw, of, sf)
-print(sinal.ltst_optmcycle)
+def connect(connection_data: tuple):
+    host, user, password, database = connection_data
+    db = mc.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=database,
+        auth_plugin='mysql_native_password'
+        )
+
+    return db
+
+
+def fix_fetch(torto: list) -> list:
+    fixed_list = []
+    for i in torto:
+        fixed_list.append(i[0])
+
+    return fixed_list
+
+
+def db_update(sinal : Trffclght, cursor, db):
+    sql = "REPLACE INTO results (id, n_phases, optm_cycle, green_times, edges) VALUES (%s, %s, %s, %s, %s)"
+    val = []
+    for i in range(len(sinal.edges)):
+        val.append(tuple((
+            str(i + 1),
+            str(sinal.n_phases),
+            str(sinal.optm_cycle),
+            str(sinal.green_times[i]),
+            str(sinal.edges[i]))
+        ))
+    cursor.executemany(sql, val)
+    db.commit()
+    print(cursor.rowcount, "record inserted.")
+def main():
+    #initial setup
+    connection_data = ("localhost", "root", "sapos123", "teste")
+    db_gt = connect(connection_data)
+    cursor_selector = db_gt.cursor(buffered=True)
+
+    cursor_selector.execute("SELECT observed_flow FROM traffic_light")
+    of = fix_fetch(cursor_selector.fetchall())
+    #print(of)
+
+    cursor_selector.execute("SELECT num_phases FROM traffic_light")
+    n_phases = cursor_selector.fetchone()[0]
+    #print(n_phases)
+
+    cursor_selector.execute("SELECT critical_flow_total FROM traffic_light")
+    all_crtclflw = cursor_selector.fetchone()[0]
+
+    #defines the saturation flow list
+    sf = [] #1800 default
+    for i in range(len(of)):
+        sf.append(sf_default)
+
+    cursor_selector.execute("SELECT edge_id FROM traffic_light")
+    edges = fix_fetch(cursor_selector.fetchall())
+    #print(edges)
+
+    #sets the trafficlight object
+    sinal = Trffclght(n_phases, all_crtclflw, of, sf, edges, all_red = 12)
+    sinal.update(all_crtclflw, of, sf)
+    #print(sinal.optm_cycle)
+    #print(sinal.green_times)
+
+    cursor_selector.close()
+    db_gt.close()
+
+    # saving the data
+    connection_data = ("localhost", "sapo", "1234", "traffic_results")
+    db_in = connect(connection_data)
+    cursor_in = db_in.cursor(buffered=True)
+
+    db_update(sinal, cursor_in, db_in)
+
+    cursor_in.close()
+    db_in.close()
+
+
+if __name__ == '__main__':
+    main()
